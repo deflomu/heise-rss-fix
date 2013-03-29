82eac3e2f1d09f92200b60b095a0f9d2dbf2bf0c
elm <elm@skweez.net>
2013-03-29 16:23:26 +0100

    Some refactoring

--- feed.php
+++ feed.php
@@ -27,7 +27,7 @@
 header("Content-type: text/xml; charset=UTF-8");
 define('CACHEFOLDER','feed');
 define('FEEDINTERVAL',600);
-define('MAXARTIKELS',100);
+define('MAXARTICLES',100);
 
 $feedUrl = urlencode('http://www.heise.de/newsticker/heise-atom.xml');
 
@@ -35,18 +35,27 @@ if( isset($_GET["url"]) ) {
     $feedUrl = urlencode($_GET["url"]);
 }
 
+# This script can be called with the option do and the values reload or sync 
+# (feed.php?do=reload). "reload" forces a complete reload of the feed source 
+# and the content of every feed item from the website. "sync" does only force
+# the feed source to be reloaded but not the content of the feed items from the 
+# website.
 $do_reload = false;
 $do_sync = false;
-$do_removeDescription = false;
-
 if( isset($_GET['do']) ) {
     if( $_GET["do"]=="reload" ) $do_reload = true;
     if( $_GET["do"]=="sync" ) $do_sync = true;
-    if( $_GET["do"]=="removeDescription" ) $do_removeDescription = true;
 }
 
 # Do not diplay xml errors
 libxml_use_internal_errors(true);
+
+$articlesPath = CACHEFOLDER . '/articles/';
+# Create cache folder if it does not exist
+if (!is_dir(CACHEFOLDER)) {
+    mkdir(CACHEFOLDER);
+    mkdir($articlesPath);
+}
 
 function getCurrentPageUrl() {
     $pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
@@ -66,15 +75,17 @@ function getID($string) {
 }
 
 function getArticle($url, $id, $date, $forceReload) {
-    if(!$forceReload and file_exists(CACHEFOLDER."/".$date."-".$id.".txt")) {
-        $articleFertig = file_get_contents(CACHEFOLDER."/".$date."-".$id.".txt");
+    global $articlesPath;
+
+    if(!$forceReload and file_exists($articlesPath.$id.".txt")) {
+        $article = file_get_contents($articlesPath.$id.".txt");
     } else {
         $doc = new DOMDocument();
         $article = mb_convert_encoding(file_get_contents($url), 'HTML-ENTITIES', "UTF-8");
         $doc->loadHTML($article);
         $divs = $doc->getElementsByTagName('div');
 
-        $articleFertig = 'Konnte Articleinhalt nicht finden.';
+        $article = 'Konnte Artikelinhalt nicht finden.';
 
         $newDoc = new DOMDocument();
         foreach ( $divs as $div ) {
@@ -82,19 +93,21 @@ function getArticle($url, $id, $date, $forceReload) {
                 $newDoc->appendChild($newDoc->importNode($div,true));
             }
         }
-        #TODO: Do not save HTML to read it back in later.
-        $articleFertig = $newDoc->saveHTML();
-        $articleFertig = strip_tags($articleFertig, '<span><a><pre><b><br><em><ul><li><hr><p><img><strong><table><tbody><td><tr><object><param>');
-        $articleFertig = str_replace("\"/","\"http://www.heise.de/",$articleFertig);
-        $articleFertig = '<div xmlns="http://www.w3.org/1999/xhtml">' . $articleFertig . '</div>';
-        if(!file_put_contents(CACHEFOLDER."/".$date."-".$id.".txt",$articleFertig)) die();
+        $article = $newDoc->saveHTML();
+        $article = strip_tags($article, '<span><a><pre><b><br><em><ul><li><hr><p><img><strong><table><tbody><td><tr><object><param>');
+        $article = str_replace("\"/","\"http://www.heise.de/",$article);
+        $article = '<div xmlns="http://www.w3.org/1999/xhtml">' . $article . '</div>';
+        if(!file_put_contents($articlesPath.$id.".txt",$article)) die();
     }
-    $files = glob(CACHEFOLDER."/*.txt");
-    while(count($files) > MAXARTIKELS) {
-        unlink($files[2]);
-        $files = glob(CACHEFOLDER."/*.txt");
+
+    # Delete lowest id files from cache folder
+    $files = glob($articlesPath . '*.txt');
+    while(count($files) > MAXARTICLES) {
+        unlink($files[0]);
+        unset($files[0]);
+        $files = array_values($files);
     }
-    return $articleFertig;
+    return $article;
 }
 
 $cacheFile = CACHEFOLDER."/".$feedUrl.".txt";
@@ -110,19 +123,21 @@ if($do_reload or $do_sync or !file_exists($cacheFile) or time() - filemtime($cac
         $entry = $entries->item($i);
         $url = $entry->getElementsByTagName('guid')->item(0)->nodeValue;
         $date = $entry->getElementsByTagName('pubDate')->item(0)->nodeValue;
-    
-        # Get the id
+
+        # Get the post id
         $id = getID($entry->getElementsByTagName('guid')->item(0)->nodeValue);
 
         $forceReload = false;
+        # If we find the word "update" in the title of a post we reload the 
+        # article from the source
         if($do_reload or preg_match("/update/i",$entry->getElementsByTagName('title')->item(0)->nodeValue)) {
             $forceReload = true;
         }
 
-        if($do_removeDescription) {
-            # Remove description element
-            $entry->removeChild($entry->getElementsByTagName('description')->item(0));
-        }
+        # Remove description element as nearly all feed readers do not show the 
+        # content if there is a discription element
+        $entry->removeChild($entry->getElementsByTagName('description')->item(0));
+
         # Remove <content:encoded> element
         $content = $entry->getElementsByTagNameNS('http://purl.org/rss/1.0/modules/content/', 'encoded')->item(0);
         if($content) {
